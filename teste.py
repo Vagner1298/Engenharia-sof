38db69ec8485283549b63c7bfd5777639d76222c
Vagner1298 <104109676+Vagner1298@users.noreply.github.com>
2022-06-24 20:25:02 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,20 +1,48 @@
 import unittest
 
-class TestStringMethods(unittest.TestCase):
+class Calculadora:
+    def soma(self, a, b):
+        return a + b
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def sub(self, a, b):
+        return a - b
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def mult(self, a, b):
+        return a * b
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def div(self, a, b):
+        if b != 0:
+            return a / b
 
-if __name__ == '__main__':
+class Teste(unittest.TestCase):
+
+    def setup(self):
+        self.Calculadora = ('Calculadora')
+
+    def teste_soma(self):
+        self.calc = Calculadora()
+        resultado = self.calc.soma(2, 5)
+        esperado = 7
+        self.assertEqual(resultado, esperado)
+
+    def teste_sub(self):
+        self.calc = Calculadora()
+        resultado = self.calc.sub(18, 12)
+        esperado = 6
+        self.assertEqual(resultado, esperado)
+
+    def teste_multi(self):
+        self.calc = Calculadora()
+        resultado = self.calc.mult(5, 5)
+        esperado = 25
+        self.assertEqual(resultado, esperado)
+
+    def teste_div(self):
+        self.calc = Calculadora()
+        resultado = self.calc.div(24, 2)
+        esperado = 12
+        self.assertEqual(resultado, esperado)
+
+
+if _name_ == '_main_':
     unittest.main()
